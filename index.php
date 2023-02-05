12d97ad5a5047695b68a35410245c832c8da1355
Karl Roxas <supercrane21@protonmail.com>
2023-02-05 20:45:44 +0800

    add overflow-x styling

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">	
 	<title>BBQ Blog</title>
 	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="style.css">
 	<link rel="shortcut icon" type="image/jpg" href="images/iconlogo.png"/>
 	<script>
 	function togglePassword(btn) {
