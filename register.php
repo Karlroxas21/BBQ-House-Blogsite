af432b22efd1748823f7c5050e104f5bd5db7ece
Karl Roxas <supercrane21@protonmail.com>
2023-02-06 16:21:09 +0800

    Mitigate SQL Injection

--- register.php
+++ register.php
@@ -2,32 +2,32 @@
 
 if(isset($_POST['Submit'])) {
 
-	include 'config.php';
+    include 'config.php';
 
-	$name = htmlspecialchars($_POST['Name']);
-	$email = htmlspecialchars($_POST['Email']);
-	$pass = htmlspecialchars($_POST['Password']);
-	$cpassword= htmlspecialchars($_POST['cpassword']);
-	$createdDate = date("Y-m-d");
-	$modifiedDate = date("Y-m-d");
-	if ($pass == $cpassword) {
+    $name = htmlspecialchars($_POST['Name']);
+    $email = htmlspecialchars($_POST['Email']);
+    $pass = htmlspecialchars($_POST['Password']);
+    $cpassword= htmlspecialchars($_POST['cpassword']);
+    $createdDate = date("Y-m-d");
+    $modifiedDate = date("Y-m-d");
+    if ($pass == $cpassword) {
 
-	$pass = mysqli_real_escape_string($mysqli, $pass);
-	$pass = password_hash($pass, PASSWORD_DEFAULT);
+        $pass = password_hash($pass, PASSWORD_DEFAULT);
 
-	$sql="INSERT INTO tblaccounts(Name,Email,Password,CreatedDate,ModifiedDate)
-	VALUES('$name','$email','$pass','$createdDate','$modifiedDate')";
+        $stmt = $mysqli->prepare("INSERT INTO tblaccounts(Name,Email,Password,CreatedDate,ModifiedDate)
+	    VALUES(?,?,?,?,?)");
+        $stmt->bind_param("sssss", $name, $email, $pass, $createdDate, $modifiedDate);
+        $stmt->execute();
 
-	
-if (mysqli_query($mysqli, $sql)) {
-	echo "<script>alert('Account created succesfuly');
-	window.location.href= 'index.php'</script>";
-  }
-} else {
-	echo "<script>alert('The passwords you entered do not match');
-	window.location.href= 'index.php'</script>";
+        if ($stmt->affected_rows === 1) {
+            echo "<script>alert('Account created succesfuly');
+            window.location.href= 'index.php'</script>";
+        }
+    } else {
+        echo "<script>alert('The passwords you entered do not match');
+        window.location.href= 'index.php'</script>";
+    }
+    $stmt->close();
 }
-}
-
 
 ?>
