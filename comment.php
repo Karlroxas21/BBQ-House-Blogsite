66c2521ec8b2c76e8ef9fb4f542ba24069487222
Karl Roxas <supercrane21@protonmail.com>
2023-02-13 12:58:09 +0800

    sanitize and empty comment will not be accepted

--- comment.php
+++ comment.php
@@ -4,8 +4,16 @@
 	$message = htmlspecialchars($_POST['Message']);
 	$postDate = date("Y-m-d");
 
+	if($message == ""){
+		echo "<script>alert('Empty post is not allowed!');</script>";
+		echo "<script>window.location.href = 'index.php';</script>";
+		exit();
+	}
+
+	$sanitized_message = mysqli_real_escape_string($mysqli, $message);
+
 	$stmt = $mysqli->prepare("INSERT INTO tblcomments (Message, PostDate) VALUES (?,?)");
-	$stmt->bind_param("ss", $message, $postDate);
+	$stmt->bind_param("ss", $sanitized_message, $postDate);
 	$stmt->execute();
 
 	//echo "<scipt>alert('Comment added successfully.');</script>";
